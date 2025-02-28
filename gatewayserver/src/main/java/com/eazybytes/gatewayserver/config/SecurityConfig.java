8b22c4aad8a38fd734f07600dc40a33f7069da41
kykim79 <kykim79@gmail.com>
2025-02-28 22:59:36 +0900

    add code and properties about oauth2

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.eazybytes.gatewayserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+public class SecurityConfig {
+    
+}
